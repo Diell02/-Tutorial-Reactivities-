d12ed4a8993501936dc35a202e2a73524053ed3a
Diell02 <diellkepuska@gmail.com>
2022-04-30 22:46:06 +0200

    Diell
    KALOJ ERRORI
    Urime Urime Urime

---
+++ client-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+
+  const[activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>
+              {activity.title}
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
