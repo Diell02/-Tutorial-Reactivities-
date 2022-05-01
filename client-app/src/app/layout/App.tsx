b89ac40e48997904e8a09666d7ea551d472e6ebb
Diell02 <diellkepuska@gmail.com>
2022-05-02 00:53:31 +0200

    Diell
    Crud ma i avansun

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -10,6 +10,7 @@ function App() {
 
   const[activities, setActivities] = useState<Activity[]>([]);
   const[selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const[editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities').then(response => {
@@ -25,15 +26,27 @@ function App() {
     setSelectedActivity(undefined);
   }
 
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
   return (
     <>
-      <NavBar />
+      <NavBar openForm={handleFormOpen} />
       <Container style={{marginTop: '10em'}}>
         <ActivityDashboard 
           activities={activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
         />
       </Container>
     </>
