c9184a5bfc7b09e8e202ea88092f56f1396f09bb
Diell02 <diellkepuska@gmail.com>
2022-05-02 15:02:50 +0200

    Diell

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,16 +5,24 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/Dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
 import agent from '../api/Agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
 
   const[activities, setActivities] = useState<Activity[]>([]);
   const[selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const[editMode, setEditMode] = useState(false);
+  const[loading, setLoading] = useState(true);
 
   useEffect(() => {
     agent.Activities.list().then(response => {
-      setActivities(response);
+      let activities: Activity[] = [];
+      response.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+      setLoading(false);
     })
   }, [])
 
@@ -46,6 +54,8 @@ function App() {
   function handleDeleteActivity(id: string) {
     setActivities([...activities.filter(x => x.id !== id)])
   }
+
+  if(loading) return <LoadingComponent content='Loading SIMTS'/>
 
   return (
     <>
