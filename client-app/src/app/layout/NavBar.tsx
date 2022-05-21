3432174e466039f22c897f1b9721af2bf489be83
Diell02 <diellkepuska@gmail.com>
2022-05-21 15:24:34 +0200

    Diell

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
-import "./NavBar.css";
+
 export default function NavBar() {
     return(
         <Menu inverted fixed='top'>
             <Container>
                 <Menu.Item as={NavLink} to='/' exact header>
-                    <img src="/assets/logo.png" alt="logo" style={{ width: '80px'}}/>
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px', width: '80px'}}/>
                 </Menu.Item>
-                {/*<Menu.Item as={NavLink} to='/activities' name='Activities'/>*/}
-                
+                <Menu.Item as={NavLink} to='/activities' name='Activities'/>
                 <Menu.Item>
-                    <div style={{marginLeft: '660px'}}>
-                <Button as={NavLink} to='/activities' color='green' content='Nxenes' style={{marginRight: '10px'}} />
-                <Button as={NavLink} to='/activities' color='blue' content='Prind' style={{marginRight: '10px'}} />
-                <Button as={NavLink} to='/activities' color='red' content='Mesimdhenes' style={{marginRight: '10px'}} />
-                <Button as={NavLink} to='/activities' color='black' content='Staf' style={{marginRight: '10px'}} />
-                </div>
-                {/*<Button as={NavLink} to='/createActivity' color='red' content='Create Activity'/>*/}
+                <Button as={NavLink} to='/createActivity' positive content='Create Activity'/>
+                    {/*<Button.Group style={{marginLeft:'400px'}}>
+                    <Button positive content='Nxenes' style={{marginLeft:'10px'}}/>
+                    <Button positive content='Prind' style={{marginLeft:'10px'}}/>
+                    <Button positive content='Mesimdhenes' style={{marginLeft:'10px'}}/>
+                    <Button positive content='StafM' style={{marginLeft:'10px'}}/>
+                    </Button.Group>*/}
                 </Menu.Item>
             </Container>
         </Menu>
