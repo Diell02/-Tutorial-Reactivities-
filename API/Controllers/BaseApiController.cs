af5e2b99488d47259f92a2cf1b584036cfd5641a
Diell02 <diellkepuska@gmail.com>
2022-05-26 23:21:50 +0200

    Diell

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Application.Core;
 
 namespace API.Controllers
 {
@@ -11,5 +12,14 @@ namespace API.Controllers
         private IMediator _mediator;
 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if(result.IsSuccess && result.Value != null)
+                return Ok(result.Value);
+            if(result.IsSuccess && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
     }
 }
