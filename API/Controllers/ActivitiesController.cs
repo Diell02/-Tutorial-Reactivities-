cbbfc776e08d513c117a57017e060d87b13eecf9
Diell02 <diellkepuska@gmail.com>
2022-05-26 23:35:14 +0200

    Diell

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,9 +11,9 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<IActionResult> GetActivities()
         {
-            return await Mediator.Send(new List.Query());
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")]
@@ -25,7 +25,7 @@ namespace API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+            return HandleResult(await Mediator.Send(new Create.Command {Activity = activity}));
         }
 
         [HttpPut("{id}")]
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -7,12 +7,13 @@ using Domain;
 using FluentValidation;
 using MediatR;
 using Persistence;
+using Application.Core;
 
 namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest 
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
@@ -25,7 +26,7 @@ namespace Application.Activities
             }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
         private readonly DataContext _context;
             public Handler(DataContext context)
@@ -33,13 +34,15 @@ namespace Application.Activities
             _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 _context.Activities.Add(request.Activity);
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync() > 0;
 
-                return Unit.Value;
+                if(!result) return Result<Unit>.Failure("Failed to create activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
