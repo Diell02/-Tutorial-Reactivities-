b369f473ddc2d6ae70776935c5742cb44a60ec26
Diell02 <diellkepuska@gmail.com>
2022-05-28 00:39:27 +0200

    Diell

--- client-app/src/app/api/Agent.ts
+++ client-app/src/app/api/Agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity } from '../models/activity';
+import { store } from '../stores/store';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -15,10 +16,24 @@ axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
 }, (error: AxiosError) => {
-    const {data, status} = error.response!;
+    const {data, status, config} = error.response!;
     switch (status) {
         case 400:
-            toast.error('bad request');
+            if(typeof data === 'string') {
+                toast.error(data);
+            }
+            if(config.method === 'get' && data.errors.hasOwnProperty('id')) {
+                history.push('/not-found');
+            }
+            if(data.errors) {
+                const modalStateErrors = [];
+                for (const key in data.errors) {
+                    if(data.errors[key]) {
+                        modalStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modalStateErrors.flat();
+            }
             break;
         case 401:
             toast.error('unauthorised');
@@ -27,7 +42,8 @@ axios.interceptors.response.use(async response => {
             history.push('/not-found');
             break;
         case 500:
-            toast.error('server error');
+            store.commonStore.setServerError(data);
+            history.push('/server-error');
             break;
     }
     return Promise.reject(error);
