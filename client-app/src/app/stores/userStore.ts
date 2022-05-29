5cc9ae51861929ee8fdf5bb9cdf5d256d322d71d
Diell02 <diellkepuska@gmail.com>
2022-05-29 21:13:19 +0200

    Diell
    13/22+1

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -21,6 +21,7 @@ export default class UserStore {
             store.commonStore.setToken(user.token);
             runInAction(() => this.user = user);
             history.push('/activities');
+            store.modalStore.closeModal();
         } catch (error) {
             throw error;
         }
@@ -39,6 +40,18 @@ export default class UserStore {
             runInAction(() => this.user = user);
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    register = async (creds: UserFormValues) => {
+        try {
+            const user = await agent.Account.register(creds);
+            store.commonStore.setToken(user.token);
+            runInAction(() => this.user = user);
+            history.push('/activities');
+            store.modalStore.closeModal();
+        } catch (error) {
+            throw error;
         }
     }
 }
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Container, Header, Image, Segment } from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
+import LoginForm from '../users/LoginForm';
+import RegisterForm from '../users/RegisterForm';
 
 export default observer(function HomePage() {
-    const {userStore} = useStore(); 
+    const { userStore, modalStore } = useStore(); 
     return(
        <Segment inverted textAlign='center' vertical className='masthead'>
            <Container text>
@@ -22,9 +24,14 @@ export default observer(function HomePage() {
                     </>
                 
                 ) : (
-                    <Button as={Link} to='/login' size='huge' inverted>
-                        Login
-                    </Button>
+                    <>
+                        <Button onClick={() => modalStore.openModal(<LoginForm />)} size='huge' inverted>
+                            Login
+                        </Button>
+                        <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='huge' inverted>
+                            Register
+                        </Button>
+                    </>
                 )}
                
            </Container>
