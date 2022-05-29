fdb7a7ad6758dc2d629fd123798a0ce012609388
Diell02 <diellkepuska@gmail.com>
2022-05-29 23:59:14 +0200

    Diell

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,13 +2,11 @@ using System;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
 
     public class ActivitiesController : BaseApiController
     {
