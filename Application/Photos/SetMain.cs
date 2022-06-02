ca0f93549f249d52472944780d0bf9d1a7a8c9d5
Diell02 <diellkepuska@gmail.com>
2022-06-02 20:51:11 +0200

    Diell

--- Application/Photos/SetMain.cs
+++ Application/Photos/SetMain.cs
@@ -9,7 +9,7 @@ using Persistence;
 
 namespace Application.Photos
 {
-    public class Delete
+    public class SetMain
     {
         public class Command : IRequest<Result<Unit>>
         {
@@ -19,19 +19,17 @@ namespace Application.Photos
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
-            private readonly IPhotoAccessor _photoAccessor;
             private readonly IUserAccessor _userAccessor;
-            public Handler(DataContext context, IPhotoAccessor photoAccessor, IUserAccessor userAccessor)
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
                 _userAccessor = userAccessor;
-                _photoAccessor = photoAccessor;
                 _context = context;
             }
 
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var user = await _context.Users.Include(p => p.Photos)
-                .FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+                    .FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
 
                 if (user == null) return null;
 
@@ -39,19 +37,17 @@ namespace Application.Photos
 
                 if (photo == null) return null;
 
-                if(photo.IsMain) return Result<Unit>.Failure("You cannot delete your main photo");
+                var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
 
-                var result = await _photoAccessor.DeletePhoto(photo.Id);
+                if (currentMain != null) currentMain.IsMain = false;
 
-                if (result == null) return Result<Unit>.Failure("Problem deleting photo from Cloudinary");
-
-                user.Photos.Remove(photo);
+                photo.IsMain = true;
 
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return Result<Unit>.Success(Unit.Value);
 
-                return Result<Unit>.Failure("Problem deleting photo from API");
+                return Result<Unit>.Failure("Problem setting main photo");
             }
         }
     }
