d37ab7f186005369b58aadb9fb244816be3005de
Diell02 <diellkepuska@gmail.com>
2022-06-02 14:00:11 +0200

    Diell

---
+++ Infrastructure/Photos/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+namespace Infrastructure.Photos
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string Apikey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
