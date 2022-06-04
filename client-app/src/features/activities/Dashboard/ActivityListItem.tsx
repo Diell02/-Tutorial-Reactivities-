79f2bfa44495ebcf99d096f371d723d61f80061a
Diell02 <diellkepuska@gmail.com>
2022-06-04 23:02:18 +0200

    Diell

--- client-app/src/features/activities/Dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/Dashboard/ActivityListItem.tsx
@@ -24,7 +24,7 @@ export default function ActivityListItem({activity}: Props) {
                             <Item.Header as={Link} to={`/activities/${activity.id}`}>
                                 {activity.title}
                             </Item.Header>
-                            <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
+                            <Item.Description>Hosted by <Link to={`/profiles/${activity.hostUsername}`}>{activity.host?.displayName}</Link></Item.Description>
                             {activity.isHost && (
                                 <Item.Description>
                                     <Label basic color='orange'>
