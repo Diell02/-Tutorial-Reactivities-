c4f6248539759aa01bd153494d29d982c49b726a
Diell02 <diellkepuska@gmail.com>
2022-06-12 11:57:58 +0200

    Diell

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,6 +2,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Application.Core;
+using API.Extensions;
 
 namespace API.Controllers
 {
@@ -19,6 +20,20 @@ namespace API.Controllers
             if(result.IsSuccess && result.Value != null)
                 return Ok(result.Value);
             if(result.IsSuccess && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
+
+        protected ActionResult HandlePagedResult<T>(Result<PagedList<T>> result)
+        {
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null)
+            {
+                Response.AddPaginationHeader(result.Value.CurrentPage, result.Value.PageSize,
+                    result.Value.TotalCount, result.Value.TotalPages);
+                return Ok(result.Value);
+            }    
+            if (result.IsSuccess && result.Value == null)
                 return NotFound();
             return BadRequest(result.Error);
         }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -8,14 +8,18 @@ using Application.Core;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Application.Interfaces;
+using System.Linq;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<ActivityDto>>> {}
+        public class Query : IRequest<Result<PagedList<ActivityDto>>> 
+        {
+            public PagingParams Params { get; set; }
+        }
 
-        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
+        public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -27,14 +31,18 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await _context.Activities
+                var query =  _context.Activities
+                    .OrderBy(d => d.Date)
                     .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, 
                         new {currentUsername = _userAccessor.GetUsername()})
-                    .ToListAsync(cancellationToken);
-
-                return Result<List<ActivityDto>>.Success(activities);
+                    .AsQueryable();
+               
+                return Result<PagedList<ActivityDto>>.Success(
+                    await PagedList<ActivityDto>.CreateAsync(query, request.Params.PageNumber, 
+                    request.Params.PageSize)
+                );
             }
         }
     }
